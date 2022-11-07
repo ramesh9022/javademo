e4e0925fe6ffbe6789a1ad8d6e25deea74a48880
ramesh9022 <rameshravte2001@gmail.com>
2022-11-07 11:03:14 +0530

    create

--- sham.java
+++ sham.java
@@ -1,6 +1,14 @@
+<<<<<<< Updated upstream
 public class y{
 public static void main(String[]args){
 
 Sytem.out.printnl("class");
 }
 }
+=======
+public class s{
+public static void main(String[]args){
+System.out.printnl("s");
+}
+}
+>>>>>>> Stashed changes
